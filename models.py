01b4c9ded2a5cd3978447f7a23db5ac075db1879
Dennis Segall <segalldennis@gmail.com>
2021-08-15 17:15:58 -0500

    redirected database to schema.sql and fixed typos

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ ROOT = path.dirname(path.relpath((__file__)))
 def create_post(name, content):
     con = sql.connect(path.join(ROOT, 'database.db'))
     cur = con.cursor()
-    cur.execute('insert into posts (name, comtemt) values(?, ?)', (name, content))
+    cur.execute('insert into posts (name, content) values(?, ?)', (name, content))
     con.commit()
     con.close()
 
